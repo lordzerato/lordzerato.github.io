2996da5140799c49653caffeadd0167d1b849eaf
Fauzi Ramadan <fauzidanrama@gmail.com>
2020-11-21 11:45:31 +0700

    Reset with DOM and some adjustment CSS

--- scripts.js
+++ scripts.js
@@ -8,11 +8,14 @@ function drag(ev){
 
 function drop(ev){
     var data = ev.dataTransfer.getData("Text");
-    var classDiv = ev.target.getAttribute("class");
+    var namaDiv = ev.target.getAttribute("name");
+    document.getElementById(data).removeAttribute("class")
     ev.target.appendChild(document.getElementById(data));
     document.getElementById(data).setAttribute("draggable", false);
-    ev.target.setAttribute("ondragover", "");
-    document.getElementById(classDiv).setAttribute("value", data);
+    document.getElementById(data).setAttribute("width", "110px");
+    document.getElementById(data).setAttribute("height", "110px");
+    ev.target.removeAttribute("ondragover");
+    document.getElementById(namaDiv).setAttribute("value", data);
 }
 
 function check(){
@@ -30,6 +33,38 @@ function check(){
     return true;
 }
 
+let arrayAngka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function random (arr) {
+    let tmp = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let acakNomer = Math.floor(Math.random()*9);
+        tmp = arr[i];
+        arr[i] = arr[acakNomer];
+        arr[acakNomer] = tmp;
+    }
+
+    return arr;
+}
+
+function resetForm(str) {
+    var arrayBaru = random(arrayAngka);
+    console.log(arrayBaru)
+    for (let i = 0; i < arrayBaru.length; i++) {
+        document.getElementById(`drag${arrayBaru[i]}`).outerHTML = "";
+        document.getElementById(`id${arrayBaru[i]}`).removeAttribute("value");
+        var gambar = document.createElement("img");
+        var targetPindah = document.getElementById("tempatAnswer");
+        targetPindah.appendChild(gambar);
+        gambar.setAttribute("id", `drag${arrayBaru[i]}`);
+        gambar.setAttribute("src", `./img/${str}${arrayBaru[i]}.PNG`);
+        gambar.setAttribute("class", "answer");
+        gambar.setAttribute("draggable", "true");
+        gambar.setAttribute("ondragstart", "drag(event)");
+        var tabel = document.getElementById(`tab${arrayBaru[i]}`);
+        tabel.setAttribute("ondragover", "allowDrop(event)");
+    }
+}
 
 let cerita = 0;
 function next() {
